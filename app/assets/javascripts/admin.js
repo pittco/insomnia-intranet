abea6f42857fe494def4a98fa25e5a9d0986746e
Jon Daniel <binarycleric@gmail.com>
2015-06-21 19:07:34 -0400

    Added new basic admin layout

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,4 @@
+//= require jquery
+//= require jquery_ujs
+//= require jquery.easing.min
+//= require bootstrap-sprockets
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,4 @@
 //= require jquery_ujs
 //= require jquery.easing.min
 //= require bootstrap-sprockets
-//= require_tree .
 
