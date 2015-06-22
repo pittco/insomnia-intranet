7994f245448b0aea46269958b31a82408a7b9a3e
Jon Daniel <binarycleric@gmail.com>
2015-06-21 22:24:07 -0400

    Check the current user instead of just the session id

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,11 +6,13 @@ class AdminController < ApplicationController
   private
 
   def require_user
-    redirect_to '/auth/google_oauth2' unless session[:user_id]
+    redirect_to '/auth/google_oauth2' unless current_user
   end
 
   helper_method def current_user
     @current_user ||= User.find(session[:user_id])
+  rescue
+    nil
   end
 
 end
