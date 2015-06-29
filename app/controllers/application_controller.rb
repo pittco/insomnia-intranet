08be5638b0754fed9f0667d51a2fe745ceef578f
Colin Dean <colindean@users.noreply.github.com>
2015-06-29 00:10:27 -0400

    Populates @event_name from EVENT_NAME envvar with default "Pittco"
    I think I did this right.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def get_event_name
+    @event_name = ENV['EVENT_NAME'] || "Pittco"
+  end
+  
+  before_filter :get_event_name
+  
 end
