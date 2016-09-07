1d31dd81e3581aaa47f85fbbf6f88a65c3712f41
Jon Daniel <binarycleric@gmail.com>
2016-09-07 19:17:35 -0400

    Added LETSENCRYPT_CHALLENGE as an env var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "J1bUc31Qq9hUAxPK-JgypeWm_eve0dyjK9XwTNkpZrU._VcKg87FD12KJ-tmkmQbs4I2vH2Yb_u3YGbeGfbNarw"
+    render text: ENV['LETSENCRYPT_CHALLENGE']
   end
 end
