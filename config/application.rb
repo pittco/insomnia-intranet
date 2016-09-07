ebeaac1c6e06c5fd41fa7e80981fe828ff1634ec
Jon Daniel <binarycleric@gmail.com>
2016-09-07 19:26:51 -0400

    %w not %i

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Intranet
     # config.i18n.default_locale = :de
 
     config.middleware.use Rack::SslEnforcer,
-      only_environments: %i( production ),
+      only_environments: %w( production ),
       hsts: { expires: 86400 }
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
